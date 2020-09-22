f5d615429cad592bcd267164941708e189433a22
Faustin <fukundimana@gmail.com>
2020-09-22 16:07:26 +0200

    ft(AuthenticationApi)

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import blogsRouter from './routes/blogs';
 import UsersRouter from './routes/index';
 import contactRouter from './routes/contact';
 import mongoose from 'mongoose';
+import authRoutes from './routes/authRoutes';
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 // MongoDB Connection
 const dbURI = "mongodb+srv://faustin:faustin@learnnode.e3lxu.mongodb.net/nodeDB?retryWrites=true&w=majority";
@@ -18,13 +22,44 @@ db.on('error', () => {
 });
 
 const app = express();
+// middleware
+app.use(express.static('public'));
 app.use(express.json());
+// view engine
+app.set('view engine', 'ejs');
+
 const PORT = 2000;
 app.use('/blogs', blogsRouter);
 app.use('/', UsersRouter);
-app.use('/questions', contactRouter)
+app.use('/questions', contactRouter);
+app.use(authRoutes);
  
 app.listen(PORT, () =>{
     console.log(`App is listening to port:${PORT}`);
 });
+
+// cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// app.get('/set-cookies', (req, res) => {
+
+//   // res.setHeader('Set-Cookie', 'newUser=true');
+  
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+//   res.send('you got the cookies!');
+
+// });
+
+// app.get('/read-cookies', (req, res) => {
+
+//   const cookies = req.cookies;
+//   console.log(cookies.newUser);
+
+//   res.json(cookies);
+
+// });
+ 
 
