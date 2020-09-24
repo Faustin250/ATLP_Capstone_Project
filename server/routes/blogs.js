e2d27ae1e9df85828015ee2b28bbbf63df421bb5
Faustin <fukundimana@gmail.com>
2020-09-24 18:43:46 +0200

    made some changes

--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import Blog from '../models/blog';
 import blogsController from '../controllers/blogsController';
- 
-// import getSingleBlog from '../middlewares/getSingleBlog';
-
 const router = express.Router();
 // Create a blog
 router.post('/newBlog', blogsController.createOne);
