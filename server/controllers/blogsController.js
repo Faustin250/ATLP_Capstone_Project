058aef54af3f18d729e3c611e75b50c7ef25b39d
Faustin <fukundimana@gmail.com>
2020-09-29 09:44:58 +0200

    Made some changes

--- server/controllers/blogsController.js
+++ server/controllers/blogsController.js
@@ -1,13 +1,25 @@
-
 import Blog from '../models/blog';
-export default class BlogsController {
+import mongoose from 'mongoose';
 
-    static findOne(req, res) {
-        res.json({
-            message: 'success',
-            data: res.blog,
+exports.blogs_get_one = (req, res, next) => {
+  const id = req.params.blogId
+  Blog.findById(id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: 'No valid entry found for that id'
         });
-    }
-       
-  }
-    
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      });
+    });
+}
+ 
--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,28 +1,10 @@
 import express from 'express';
 import Blog from '../models/blog';
 import blogsController from '../controllers/blogsController';
+import mongoose from 'mongoose';
 
 const router = express.Router();
  
-// Get single blog
-router.get('/:id', getBlog, blogsController.findOne);
-
-// Get One middleware
-async function getBlog(req, res, next) {
-    let blog;
-    try{
-     blog = await Blog.findById(req.params.id);
-     if(blog == null){
-         return res.status(404).json({message: 'Blog not found'});
-     }
-    } catch(err) {
-        return res.status(500).json({
-            message: err.message,
-        });
-    }
-    res.blog = blog;
-    next();
-}
-
-
+router.get('/:blogId', blogsController.blogs_get_one);
+ 
 module.exports = router;
